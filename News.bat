9ab0fc4358d23a51ac4f3b86b516fff3a1eeec8e
Swarmed2674 <96339124+Swarmed2674@users.noreply.github.com>
2022-11-21 21:01:58 +0800

    Add files via upload

--- News.bat
+++ News.bat
@@ -1,11 +1,11 @@
 @echo off
-echo NEWS SakuraOSv4 以後改為 OneOS !?
+echo NEWS SakuraOSv4 �H��אּ OneOS !?
 echo.
-echo Kiawm91/報導
+echo Kiawm91/����
 echo.
-echo 最近，Sakura Inc表示 SakuraOS自V4後改為OneOS
-echo V3也被確定製作！
-echo 但這次V4與V3一同發布
-echo 請各位敬請期待！
+echo �̪�ASakura Inc���� SakuraOS��V4��אּOneOS
+echo V3�]�Q�T�w�s�@�I
+echo ���o��V4�PV3�@�P�o��
+echo �ЦU��q�д��ݡI
 echo.
 echo Sakura Inc.
