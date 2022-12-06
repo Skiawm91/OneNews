bf1185f205bc1a3a081ac9955d1cd1123e2098f5
Swarmed2674 <96339124+Swarmed2674@users.noreply.github.com>
2022-12-06 22:10:01 +0800

    Add files via upload

--- News.bat
+++ News.bat
@@ -1,11 +1,15 @@
 @echo off
-echo NEWS SakuraOSv4 以後改為 OneOS !?
-echo.
+echo NEWS SakuraOSv3取消開發！被Sakura OneOS取代
 echo Kiawm91/報導
 echo.
-echo 最近，Sakura Inc表示 SakuraOS自V4後改為OneOS
-echo V3也被確定製作！
-echo 但這次V4與V3一同發布
-echo 請各位敬請期待！
+echo Sakura Inc. 最終公布了開發計畫
+echo SakuraOSv3被取消掉了！
+echo SakuraOS 是一款非常棒的系統對吧...
+echo 可惜啊！最終沒能看到 Rampabe架構
+echo 最近由Sakura團隊製作的OneOS 是什麼呢
 echo.
-echo Sakura Inc.
+echo Sakura OneOS
+echo 最大功能就是支援更新！再也不用手動下載啦！
+echo  SakuraOS API 1.0 是一個什麼樣的API呢?
+echo SakuraOS v2可以無痛升級！！！
+echo 請各位敬請期待！
