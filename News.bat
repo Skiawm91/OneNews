d222805fbceac00dd6696efb244b02ef6e5ac99a
Swarmed2674 <96339124+Swarmed2674@users.noreply.github.com>
2023-05-10 21:37:50 +0800

    23/5/10 新聞

--- News.bat
+++ News.bat
@@ -1,14 +1,23 @@
 @echo off
-echo OneOS發布，現在的開始畫面更現代?
-echo SKiawm91/報導
-echo.
-echo 根據最新版本的OneOS (3.00.391) 的DEV版
-echo 開始畫面從原本的偽磁磚變成按鈕磁磚(似)
-echo 希望這個系統不要又取消掉吧...
-echo.
-echo SakuraOSv4 即將製作，居然是已經取消的Rampabe!?
-echo Guoy2674/報導 (已翻譯為繁體中文)
-echo 根據Sakura Inc.的消息，SakuraOSv4將只有Basic版
-echo 這個版本我預計不會有Update功能
-echo 我預計應該有可能與我洩漏出的V3一致
-echo by Guoy2674 (Fake DEV)
+echo :------------------------:
+echo :本次新聞報導 (2023/5/10) :
+echo :------------------------:-----------------:
+echo : OneOS 3.00.652 更新一堆東西，帶你來看看！	: 
+echo : SKiawm91/報導					:
+echo :							:
+echo : 根據最新內部消息，這個版本完善許多		:
+echo : 內容，由Skiawm91製作的登入畫面更現代化	:
+echo : 這個系統的Patch由LzSyp1923研發與開發，為	:
+echo : OneOS提供重大幫助！				:
+echo :							:
+echo :							:
+echo : OneOS 3.00.652 已知的更新內容有：		:
+echo : OOBE創建用戶終於不用一定得創個帳戶了！	:
+echo : 全新簡潔化的登入畫面				:
+echo : 控制台更新圖形化					:
+echo : 開始按鈕現身！					:
+echo :							:
+echo : 而已知被修復的"特性"有：				:
+echo : Command模式的綠字黑底影響到主題		:
+echo : SysMGR系統資訊修復顯示問題			:
+echo -------------------------------------------:
