a3c5e7b2180a1004c8d206031751144c52dfcbfe
Swarmed2674 <96339124+Swarmed2674@users.noreply.github.com>
2023-05-10 21:41:24 +0800

    剛剛那個壞了

--- News.bat
+++ News.bat
@@ -1,23 +1,23 @@
 @echo off
 echo :------------------------:
-echo :本次新聞報導 (2023/5/10) :
+echo :本次新聞報導 (2023/5/10):
 echo :------------------------:-----------------:
-echo : OneOS 3.00.652 更新一堆東西，帶你來看看！	: 
-echo : SKiawm91/報導					:
-echo :							:
-echo : 根據最新內部消息，這個版本完善許多		:
-echo : 內容，由Skiawm91製作的登入畫面更現代化	:
-echo : 這個系統的Patch由LzSyp1923研發與開發，為	:
-echo : OneOS提供重大幫助！				:
-echo :							:
-echo :							:
-echo : OneOS 3.00.652 已知的更新內容有：		:
-echo : OOBE創建用戶終於不用一定得創個帳戶了！	:
-echo : 全新簡潔化的登入畫面				:
-echo : 控制台更新圖形化					:
-echo : 開始按鈕現身！					:
-echo :							:
-echo : 而已知被修復的"特性"有：				:
-echo : Command模式的綠字黑底影響到主題		:
-echo : SysMGR系統資訊修復顯示問題			:
+echo : OneOS 3.00.652 更新一堆東西，帶你來看看！: 
+echo : SKiawm91/報導				:
+echo :					:
+echo : 根據最新內部消息，這個版本完善許多	:
+echo : 內容，由Skiawm91製作的登入畫面更現代化:
+echo : 這個系統的Patch由LzSyp1923研發與開發，為:
+echo : OneOS提供重大幫助！			:
+echo :					:
+echo :					:
+echo : OneOS 3.00.652 已知的更新內容有：	:
+echo : OOBE創建用戶終於不用一定得創個帳戶了！:
+echo : 全新簡潔化的登入畫面			:
+echo : 控制台更新圖形化			:
+echo : 開始按鈕現身！			:
+echo :					:
+echo : 而已知被修復的"特性"有：		:
+echo : Command模式的綠字黑底影響到主題	:
+echo : SysMGR系統資訊修復顯示問題		:
 echo -------------------------------------------:
