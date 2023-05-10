032f5c43e2722725544beab079efcb897b592bc9
Swarmed2674 <96339124+Swarmed2674@users.noreply.github.com>
2023-05-10 21:54:14 +0800

    艸

--- News.bat
+++ News.bat
@@ -3,7 +3,7 @@ echo :------------------------:
 echo :本次新聞報導 (2023/5/10):
 echo :------------------------:-----------------
 echo : OneOS 3.00.652 更新一堆東西，帶你來看看！ 
-echo : SKiawm91/報導				      
+echo : SKiawm91、LzSyp1923/報導				      
 echo :					            
 echo : 根據最新內部消息，這個版本完善許多	      
 echo : 內容，由Skiawm91製作的登入畫面更現代化     
@@ -19,5 +19,11 @@ echo : 開始按鈕現身！
 echo :					            
 echo : 而已知被修復的"特性"有：		            
 echo : Command模式的綠字黑底影響到主題	      
-echo : SysMGR系統資訊修復顯示問題		      
+echo : SysMGR系統資訊修復顯示問題
+echo :		      
+echo : 偷偷跟你說
+echo : 我這次製作了一個Patch
+echo : Kernel核心版本目前為Pre 0.4
+echo : 屬於：Kernel根本沒做好，只做好安裝程序XD
+echo : 所以，請期待:D 
 echo -------------------------------------------
